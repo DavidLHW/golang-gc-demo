a2036045925231a1ed8dca3ce7184fe6ee83f683
David Lin <13061926+DavidLHW@users.noreply.github.com>
2022-11-23 05:38:55 +0800

    feat: init gin

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 func main() {
-	// ...
+	r := SetUpRouter()
+	r.Run(":8080")
+}
+
+func SetUpRouter() *gin.Engine {
+	r := gin.New()
+	return r
 }
--- main_test.go
+++ main_test.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"os"
 	"testing"
+
+	"github.com/gin-gonic/gin"
 )
 
-func TestMain(t *testing.T) {
-	// ...
+func TestMain(m *testing.M) {
+	os.Setenv("GOMEMLIMIT", "30MiB")
+	os.Setenv("GOGC", "100")
+	gin.SetMode(gin.TestMode)
+	os.Exit(m.Run())
 }

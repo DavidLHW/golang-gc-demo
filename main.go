40be3bef0d5fd0206d7e1f36200dfa3f47a859b3
David Lin <13061926+DavidLHW@users.noreply.github.com>
2022-11-23 05:40:42 +0800

    feat: GET config route

--- main.go
+++ main.go
@@ -1,13 +1,41 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+type configJson struct {
+	GOMEMLIMIT string `json:"gomemlimit"` // in human bytes
+	GOGC       string `json:"gogc"`       // in integer percentage
+}
 
 func main() {
-	r := SetUpRouter()
+	r := SetUpRouter(true)
 	r.Run(":8080")
 }
 
-func SetUpRouter() *gin.Engine {
+func SetUpRouter(withRoutes bool) *gin.Engine {
 	r := gin.New()
+	if withRoutes {
+		r.GET("/config", readConfigHandler)
+	}
 	return r
+}
+
+func getConfigJson() configJson {
+	var configJson configJson
+
+	configJson.GOMEMLIMIT = os.Getenv("GOMEMLIMIT")
+	configJson.GOGC = os.Getenv("GOGC")
+	return configJson
+}
+
+func readConfigHandler(c *gin.Context) {
+	currentConfig := getConfigJson()
+	log.Printf("current config: %+v", currentConfig)
+	c.IndentedJSON(http.StatusOK, currentConfig)
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
@@ -12,4 +15,68 @@ func TestMain(m *testing.M) {
 	os.Setenv("GOGC", "100")
 	gin.SetMode(gin.TestMode)
 	os.Exit(m.Run())
+}
+
+func TestGetConfigJson(t *testing.T) {
+	test := struct {
+		want configJson
+	}{
+		want: configJson{
+			GOMEMLIMIT: "30MiB",
+			GOGC:       "100",
+		},
+	}
+
+	originalGOGC := os.Getenv("GOGC")
+	defer os.Setenv("GOGC", originalGOGC)
+
+	originalMemoryLimit := os.Getenv("GOMEMLIMIT")
+	defer os.Setenv("GOMEMLIMIT", originalMemoryLimit)
+
+	os.Setenv("GOGC", test.want.GOGC)
+	os.Setenv("GOMEMLIMIT", test.want.GOMEMLIMIT)
+
+	got := getConfigJson()
+
+	if got != test.want {
+		t.Errorf("TestGetConfigJson: want %q, got %q", test.want, got)
+	}
+}
+
+func TestReadConfigHandler(t *testing.T) {
+	test := struct {
+		want configJson
+	}{
+		want: configJson{
+			GOMEMLIMIT: "30MiB",
+			GOGC:       "100",
+		},
+	}
+
+	originalGOGC := os.Getenv("GOGC")
+	defer os.Setenv("GOGC", originalGOGC)
+
+	originalMemoryLimit := os.Getenv("GOMEMLIMIT")
+	defer os.Setenv("GOMEMLIMIT", originalMemoryLimit)
+
+	os.Setenv("GOGC", test.want.GOGC)
+	os.Setenv("GOMEMLIMIT", test.want.GOMEMLIMIT)
+
+	r := SetUpRouter(false)
+	r.GET("/config", readConfigHandler)
+
+	req, _ := http.NewRequest("GET", "/config", nil)
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+
+	responseData := w.Body.Bytes()
+	got := configJson{}
+	if err := json.Unmarshal(responseData, &got); err != nil {
+		t.Errorf("error unmarshalling response: %v", err)
+	}
+
+	if got != test.want {
+		t.Errorf("TestReadConfigHandler: want %q, got %q", test.want, got)
+	}
 }
